7cc2f78f7b69d8447d010f6420fb31b72ee2fde0
Gaev Cenne <gaev@evilparadise.com>
2021-07-29 14:07:07 +0200

    fix: correction

--- src/App.js
+++ src/App.js
@@ -8,108 +8,128 @@ import Ship from "./component/Ship/Ship.js";
 import Invader from "./component/Invader/Invader";
 
 const GAME = {
-  player: { w: 0, h: 0 },
-  invader: { w: 0, h: 0 },
-  bullet: { w: 0, h: 0 }
+	player: { w: 0, h: 0 },
+	invader: { w: 0, h: 0 },
+	bullet: { w: 0, h: 0 }
 }
 
 class App extends React.Component {
 
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.state = {
+		this.state = {
 
-      isStarted: false,
-      player: {
-        x: 4,
-        toLeft: false,
-        toRight: false,
-      },
-      invaders: [
-      ]
-    }
-  }
-
-  componentDidMount() {
-
-    document.addEventListener("keydown", e => {
-      this.handleKeyPress(e.key)
-    })
-  }
-
-  handleKeyPress = key => {
-    let player = { ...this.state.player }
-
-    switch (key) {
-      case "ArrowLeft":
-        console.log("vers la gauche")
-        player.x -= player.x - 1 < 0 ? 0 : player.x - 1;
-        break;
-      case "ArrowRight":
-        player.x += 1;
-        break;
-      case "Space":
-        console.log("Piou piou");
-        break;
-      default:
-        break;
-    }
+			isStarted: false,
+			player: {
+				x: 4,
+				speed: 10,
+				toLeft: false,
+				toRight: true
+			},
+			invaders: [
+			]
+		}
+	}
 
 
-    this.setState(prevState => {
+	componentDidMount() {
 
-      return ({
-        ...prevState,
-        player: player
-      })
-    })
-  }
+		document.addEventListener("keydown", e => {
+			this.handleKeyPress(e.key)
+		})
 
-  handleCollision = (type, entity, bullet) => {
-
-  }
+		setInterval(this.thread, 16)
+	}
 
 
-  render() {
+	thread = () => {
+		let player = { ...this.state.player }
 
-    return (
-      <div>
-        {this.state.isStarted
-          ? this.renderHome()
-          : this.state.isOver
-            ? this.renderOver()
-            :
-            <div>
+		if (player.toRight) {
+			player.x += player.speed;
+		}
 
-              {/* Afficher les Invaders */}
-              <div>
-                <Invader x="100px" y="700px" mooveToBottom />
-                <Invader x="200px" y="700px" />
-                <Invader x="300px" y="700px" />
-                <Invader x="400px" y="700px" />
-                <Invader x="500px" y="700px" />
-                <Invader x="600px" y="700px" />
-                <Invader x="700px" y="700px" />
-                <Invader x="800px" y="700px" />
-                <Invader x="900px" y="700px" />
-                <Invader x="1000px" y="700px" />
-                <Invader x="1100px" y="700px" />
-                <Invader x="1200px" y="700px" />
-                <Invader x="1300px" y="700px" />
-                <Invader x="1400px" y="700px" />
-              </div>
+		if (this.state.player.x !== player.x) {
+			this.setState(state => {
 
-              {/* Afficher le vaiseau */}
-              <Ship x="700px" style={{ backgroundColor: "black" }} />
+				return ({
+					...state,
+					player: player
+				})
+			})
+		}
+	}
 
-            </div>
+	handleKeyPress = key => {
+		let player = { ...this.state.player }
+
+		switch (key) {
+			case "ArrowLeft":
+				player.x -= player.x - 1 < 0 ? 0 : player.x - 1;
+				break;
+			case "ArrowRight":
+				player.x += 1;
+				break;
+			case "Space":
+				console.log("Piou piou");
+				break;
+			default:
+				break;
+		}
 
 
-        }
-      </div>
-    )
-  }
+		this.setState(prevState => {
+
+			return ({
+				...prevState,
+				player: player
+			})
+		})
+	}
+
+	handleCollision = (type, entity, bullet) => {
+	}
+
+	render() {
+
+		return (
+
+			<div>
+				{
+					this.state.isStarted
+						? this.renderHome()
+						: this.state.isOver
+							? this.renderOver()
+							:
+							<div>
+
+								<div>
+									<Invader x="100px" y="700px" mooveToBottom />
+									<Invader x="200px" y="700px" />
+									<Invader x="300px" y="700px" />
+									<Invader x="400px" y="700px" />
+									<Invader x="500px" y="700px" />
+									<Invader x="600px" y="700px" />
+									<Invader x="700px" y="700px" />
+									<Invader x="800px" y="700px" />
+									<Invader x="900px" y="700px" />
+									<Invader x="1000px" y="700px" />
+									<Invader x="1100px" y="700px" />
+									<Invader x="1200px" y="700px" />
+									<Invader x="1300px" y="700px" />
+									<Invader x="1400px" y="700px" />
+								</div>
+
+								<Ship x={this.state.player.x} />
+								{/* Afficher les Invaders */}
+
+								{/* Afficher le vaisseau */}
+							</div>
+				}
+			</div>
+		)
+	}
 }
 
 export default App;
