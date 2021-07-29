bf02a506f6e81719ae5f454c73edaf55f90171e1
Eliott17 <poulaineliott@gmail.com>
2021-07-29 11:42:39 +0200

    update: composant créer

--- src/component/Ship/Ship.js
+++ src/component/Ship/Ship.js
@@ -1,8 +1,15 @@
 import React from "react"
+import Entity from "../Entity/Entity";
 
+// CSS :
 import "./Ship.css"
 
-const Ship = player => 
-    <div></div>
+const Ship = p =>
 
-export default Ship
+    <Entity
+        x={p.x} y="0"
+        width="125px" heigth="80px"
+        source="./Image vaisseau2.png"
+    />
+
+export default Ship;
