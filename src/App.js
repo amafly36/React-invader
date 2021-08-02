81419ebc96a70bb257b5d600cd350929367655fe
amafly36 <amadoudjouldebarry01@gmail.com>
2021-08-02 14:53:24 +0200

    ajout des composents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react"
 
 import './App.css';
 
+// Component :
+import Player from "./component/Player.js";
+import Invader from "./component/Invader";
+import Bullet from "./component/Bullet";
+import Header from "./component/Header";
+import Landing from "./component/Landing"
+
 const GAME = {
   player: { w: 0, h: 0 },
   invader: { w: 0, h: 0 },
@@ -10,82 +17,229 @@ const GAME = {
 
 class App extends React.Component {
 
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.state = {
+		this.state = {
 
-      isStarted: false,
-      player: {
-        x: 4,
-        toLeft: false,
-        toRight: false,
-      },
-      invaders: [
-      ]
-    }
-  }
+			isStarted: false,
+			count : 0,
+			threshold : 240,
+			player: {
+				x: 97,
+				speed: 0.25,
+				toLeft: false,
+				toRight: false,
+				fire: {
+					active : false,
+					x : 0,
+					y : 0,
+				}
+			},
+			invaders: [
+			]
+		}
+	}
 
-  componentDidMount() {
+	componentDidMount() {
+		let player = { ...this.state.player }
 
-    document.addEventListener("keydown", e => {
-      this.handleKeyPress(e.key)
-    })
-  }
+		document.addEventListener("keydown", key => {
 
-  handleKeyPress = key => {
-    let player = { ...this.state.player }
+			switch (key.code) {
+				case "ArrowLeft":
+					player.toLeft = true
+					break;
+				case "ArrowRight":
+					player.toRight = true
+					break;
+				case "Space":
 
-    switch (key) {
-      case "ArrowLeft":
-        console.log("vers la gauche")
-        player.x -= player.x - 1 < 0 ? 0 : player.x - 1;
-        break;
-      case "ArrowRight":
-        player.x += 1;
-        break;
-      case "Space":
-        console.log("Piou piou");
-        break;
-      default:
-        break;
-    }
+					if (!player.fire.active) {
+						player.fire.active =true
+						player.fire.x = player.x + 2.65
+						player.fire.y = 7
+					}
+					
+					break;
+				default:
+					break;
+			}
 
+			this.setState(state => {
 
-    this.setState(prevState => {
+				return({
+					...state,
+					player : player
+				})
+			})
+		})
 
-      return ({
-        ...prevState,
-        player: player
-      })
-    })
-  }
+		document.addEventListener("keyup", key => {
+			
+			switch (key.code) {
+				case "ArrowLeft":
+					player.toLeft = false
+					break;
+				case "ArrowRight":
+					player.toRight = false
+					break;
+				default:
+					break;
+			}
 
-  handleCollision = (type, entity, bullet) => {
+			this.setState(state => {
 
-  }
+				return({
+					...state,
+					player : player
+				})
+			})
+		})
 
+		let invaders = []
+		let ID = 0
+		
+		for (let y = 0; y < 5; y++) {
 
-  render() {
+			for (let x = 0; x < 11; x++) {
 
-    return (
-      <div>
-        {this.state.isStarted
-          ? this.renderHome()
-          : this.state.isOver
-            ? this.renderOver()
-            :
-            <div>
-              {/* Afficher les Invaders */}
+				invaders.push({
+					ID : ID++,
+					alive : true,
+					x : x * 5 + 50,
+					y : y * 5 + 60,
+					toRight : true
+				})
+			}
+		}
 
-              {/* Afficher le vaisseau */}
-            </div>
+		this.setState(state => {
 
+			return ({
+				...state,
+				invaders : invaders
+			});
+		});
 
-        }
-      </div>
-    )
-  }
+		setInterval(this.thread, 4)
+	}
+
+	thread = () => {
+		let { count, player, threshold } = this.state
+		let goDown = false
+
+		count++
+		if (count >= threshold) {
+			count = 0
+			
+			this.state.invaders.map(invader => {
+				
+				if (invader.alive) {
+					invader.x += 2
+
+					if (invader.x >= GAME.window.max.w) {
+						goDown = true
+					}
+				}
+	
+				return invader
+			})
+
+			if (goDown) {
+				
+				this.state.invaders.map(invader => {
+					invader.x -= 60
+					invader.y -= 4
+
+					return invader
+				})
+			}
+
+		} this.setState({ count : count })
+
+		if (player.toRight) {
+			player.x = (player.x +player.speed) <= GAME.window.max.w ? player.x + player.speed : GAME.window.max.w
+		} else if (player.toLeft) {
+			player.x = (player.x -player.speed) >= GAME.window.min.w ? player.x - player.speed : GAME.window.min.w
+		}
+
+		if (this.state.player.x !== player.x) {
+			this.setState(state => {
+
+				return ({
+					...state,
+					player: player
+				})
+			})
+		}
+
+		if (player.fire.active) {
+
+			player.fire.y += 1
+
+			if (player.fire.y * (window.innerHeight *0.0001) > 9) {
+				player.fire.active = false
+				player.fire.x = 0
+				player.fire.y = 7
+			}
+			this.setState(state => {
+
+				return({
+					...state,
+					player : player
+				})
+			})
+		}
+	}
+
+	handleCollision = (type, entity, bullet) => {
+	}
+
+	render() {
+
+		return (
+
+			<div>
+				<Header />
+				{
+					this.state.isStarted
+						? this.renderHome()
+						: this.state.isOver
+							? <Landing />
+							:
+							<div>
+
+								<div>
+									{
+										this.state.invaders.map(invader => 
+											invader.alive
+											?
+												<Invader
+													x ={invader.x}
+													y ={invader.y}
+												/>
+											: null
+										)
+									}
+								</div>
+								
+								<Player x={this.state.player.x} />
+
+								{
+									this.state.player.fire.active
+									?
+										<Bullet
+											x ={this.state.player.fire.x}
+											y ={this.state.player.fire.y}
+										/>
+									: null
+								}
+							</div>
+				}
+			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/component/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './header.css'
+
+
+
+const Header = () => {
+    return (
+        <header>
+            <div className="banner-container">
+                <h1>Space Invaders</h1>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header 
